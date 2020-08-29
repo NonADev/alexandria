df3f5d48ecdbeb16555c9a4333713607e7dc5bfa
nonadev <wesley.vieira.lima.1999@gmail.com>
2020-08-29 01:07:12 -0300

    animação polida e renderização ajustada

--- UI/DialogBox.gd
+++ UI/DialogBox.gd
@@ -1,15 +1,15 @@
 extends Control
 
-onready var richTextLabel = $Frente/RichTextLabel
-onready var nextIndicator = $Frente/NextIndicator
-onready var tween = $Tween
-var dialog_index = 0
 export(Array, String) var dialog = [
 	'Hello thu hathere, this is a text for you readhu hathere, this is a text for you readhu hathere, this is a text for you readhu hathere, this is a text for you readhu hathere, this is a text for you readhu hathere, this is a text for you readhu hathere, this is a text for you readhu hathere, this is a text for you read.',
 	'If you have died, please, right door, otherwise, go ahead.',
 	'You dont know? ok, go back.'
 ]
-
+onready var richTextLabel = $Frente/RichTextLabel
+onready var nextIndicator = $Frente/NextIndicator
+onready var tween = $Tween
+var dialog_index = 0
+signal dialog_ended
 
 func _ready():
 	load_dialog()
@@ -22,12 +22,13 @@ func _process(delta):
 
 func load_dialog():
 	if dialog_index < dialog.size():
-		var dialog_visible_velocity = dialog[dialog_index].length() * 0.07
+		var dialog_visible_velocity = dialog[dialog_index].length() * 0.04
 		richTextLabel.bbcode_text = dialog[dialog_index]
 		richTextLabel.percent_visible = 0
 		tween.interpolate_property(richTextLabel,"percent_visible",0,1,dialog_visible_velocity,Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 		tween.start()
 	else:
+		emit_signal("dialog_ended")
 		queue_free()
 	dialog_index += 1
 
