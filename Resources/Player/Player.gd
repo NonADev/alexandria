08b37a1a7b86e9c2087b6ae64cf12863fad9b0bd
nonadev <wesley.vieira.lima.1999@gmail.com>
2020-08-31 17:05:55 -0300

    leitura do livro refatorado

--- Resources/Player/Player.gd
+++ Resources/Player/Player.gd
@@ -7,10 +7,24 @@ const ACCELERATION = 500
 const FRICTION = 600
 var motion = Vector2.ZERO
 var dialogBody = null
-var canInteract = true
+var state = MOVE
+enum {
+	MOVE,
+	DIALOG
+}
 
 
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		DIALOG:
+			dialog_state(delta)
+	
+	move_and_slide(motion)
+
+
+func move_state(delta):	
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up") 
@@ -24,20 +38,24 @@ func _physics_process(delta):
 		animatedSprite.play("Idle")
 		motion = motion.move_toward(Vector2.ZERO, delta * FRICTION)
 	
-	if canInteract && dialogBody != null && Input.is_action_just_pressed("ui_cancel"):
-		var dialogBox = dialogBody.test()
-		canInteract = false
+	if dialogBody != null && dialogBody.has_method("readBook") && Input.is_action_just_pressed("ui_cancel"):
+		var dialogBox = dialogBody.readBook()
+		animatedSprite.play("Idle")
+		state = DIALOG
 		dialogBox.connect("dialog_ended", self, "_on_dialogBox_ended")
-	
-	motion = move_and_slide(motion)
+
+
+func dialog_state(delta):
+	animatedSprite.play("Idle")
+	motion = motion.move_toward(Vector2.ZERO, delta * FRICTION)
 
 
 func _on_dialogBox_ended():
-	canInteract = true
+	state = MOVE
 
 
 func _on_Area2D_body_entered(body):
-	if body.has_method("test"):
+	if body.has_method("readBook"):
 		dialogBody = body
 
 
--- Resources/Props/Book.gd
+++ Resources/Props/Book.gd
@@ -1,10 +1,9 @@
 extends StaticBody2D
 
 
-
-func test():
+func readBook():
 	var instanceDialog = load("res://UI/DialogBox.tscn").instance()
 	instanceDialog.dialog = ["primeira frase", "segunda fase"]
-	get_parent().get_node("HUD").add_child(instanceDialog)
+	get_node("../../HUD").add_child(instanceDialog)
 	instanceDialog.start_dialog()
 	return instanceDialog
