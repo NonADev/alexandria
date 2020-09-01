38310dfeaace1567d191f44a57b27c8b2eef7b31
nonadev <wesley.vieira.lima.1999@gmail.com>
2020-09-01 10:56:07 -0300

    removido movimentação xy, reparado estrutura do livro

--- Resources/Player/Player.gd
+++ Resources/Player/Player.gd
@@ -18,27 +18,30 @@ func _physics_process(delta):
 	match state:
 		MOVE:
 			move_state(delta)
+			interactions()
 		DIALOG:
 			dialog_state(delta)
 	
 	move_and_slide(motion)
 
 
-func move_state(delta):	
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up") 
+	#input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up") 
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
 		animatedSprite.play("Run")
-		animatedSprite.flip_h = motion.x < 0
+		animatedSprite.flip_h = motion.x > 0
 		motion = motion.move_toward(input_vector * MAX_SPEED, delta * ACCELERATION)
 	else:
 		animatedSprite.play("Idle")
 		motion = motion.move_toward(Vector2.ZERO, delta * FRICTION)
-	
-	if dialogBody != null && dialogBody.has_method("readBook") && Input.is_action_just_pressed("ui_cancel"):
+
+
+func interactions():
+	if dialogBody != null && dialogBody.has_method("readBook") && Input.is_action_just_pressed("ui_accept"):
 		var dialogBox = dialogBody.readBook()
 		animatedSprite.play("Idle")
 		state = DIALOG
