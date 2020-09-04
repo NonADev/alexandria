505f47c181338a0cb13dcdbe65e546312b4aa37a
nonadev <wesley.vieira.lima.1999@gmail.com>
2020-09-03 22:32:33 -0300

    teste feito e chao colocado, gravidade adicionada

--- Resources/Player/Player.gd
+++ Resources/Player/Player.gd
@@ -19,21 +19,29 @@ func _physics_process(delta):
 		MOVE:
 			move_state(delta)
 			interactions()
+			if is_on_wall():
+				animatedSprite.play("Idle")
 		DIALOG:
 			dialog_state(delta)
-	
-	move_and_slide(motion)
+	print(is_on_floor())
+	motion = move_and_slide(motion, UP)
 
 
 func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	#input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up") 
 	input_vector = input_vector.normalized()
 	
+	if motion.y < 450:
+		motion.y += 15
+	
+	if motion.x>0:
+		animatedSprite.flip_h = true
+	elif motion.x < 0:
+		animatedSprite.flip_h = false
+		
 	if input_vector != Vector2.ZERO:
 		animatedSprite.play("Run")
-		animatedSprite.flip_h = motion.x > 0
 		motion = motion.move_toward(input_vector * MAX_SPEED, delta * ACCELERATION)
 	else:
 		animatedSprite.play("Idle")
