a79759727430039de9057573a5c6dbda66b5774d
nonadev <wesley.vieira.lima.1999@gmail.com>
2020-09-10 22:39:29 -0300

    up

--- Resources/Player/Player.gd
+++ Resources/Player/Player.gd
@@ -26,28 +26,25 @@ func _physics_process(delta):
 			
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
+	
 	motion = move_and_slide(motion, UP)
 
 
 func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector = input_vector.normalized()
-	
-	#if motion.y < 450:
-	#	motion.y += 15
+		
+	if input_vector != Vector2.ZERO:
+		#animatedSprite.play("Run")
+		motion = motion.move_toward(input_vector * MAX_SPEED, delta * ACCELERATION)
+	else:
+		#animatedSprite.play("Idle")
+		motion = motion.move_toward(Vector2.ZERO, delta * FRICTION)
 	
 	if motion.x>0:
 		animatedSprite.flip_h = false
 	elif motion.x < 0:
 		animatedSprite.flip_h = true
-		
-	if input_vector != Vector2.ZERO:
-		animatedSprite.play("Run")
-		motion = motion.move_toward(input_vector * MAX_SPEED, delta * ACCELERATION)
-	else:
-		animatedSprite.play("Idle")
-		motion = motion.move_toward(Vector2.ZERO, delta * FRICTION)
 
 
 func interactions():
